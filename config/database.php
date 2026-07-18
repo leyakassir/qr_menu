ab89648131e24045c5e1fede92d5836d864c39ab
leyakassir <leya.kassir2@gmail.com>
2026-07-18 20:19:34 +0300

    Initial project structure and skeleton

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+$conn=new mysqli('localhost','root','','qr_menu');
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container mt-5">
+    <h1>Test Page</h1>
+    <!-- If Bootstrap is working, this button will look professional (blue, rounded) -->
+    <button class="btn btn-primary">Bootstrap is Working!</button>
+</div>
+
+<?php include 'includes/footer.php'; ?>

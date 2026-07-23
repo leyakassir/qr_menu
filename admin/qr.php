f9abe1333284951ddac67de9a4cc758960064392
leyakassir <leya.kassir2@gmail.com>
2026-07-24 01:27:38 +0300

    Use relative CSS paths for all admin pages

--- admin/qr.php
+++ admin/qr.php
@@ -25,7 +25,7 @@ $public_menu_url = $protocol . "://" . $host . $base_path . "/index.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/dashboard_style.css">
+    <link rel="stylesheet" href="../assets/css/dashboard_style.css">
 </head>
 <body>
 

850fbf5416047f02bc01235499dc9d4863d68c05
leyakassir <leya.kassir2@gmail.com>
2026-07-24 00:56:55 +0300

    Fix CSS paths for all admin pages

--- admin/settings/index.php
+++ admin/settings/index.php
@@ -55,7 +55,7 @@ if ($res && $res->num_rows > 0) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?= app_url() ?>assets/css/dashboard_style.css">
+    <link rel="stylesheet" href="../../assets/css/dashboard_style.css">
 </head>
 <body>
 
